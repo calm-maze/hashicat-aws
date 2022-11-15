0a9d50956804452f22e9e534e19f9ea974755435
calm-maze <calmmaze@gmail.com>
2022-11-15 23:29:31 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "blake-meisenheimer"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/tfchip-aws/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "blake-meisenheimer"
 }
